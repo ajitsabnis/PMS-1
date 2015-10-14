afa8e3503ec35768ea9df6ecf4a79ce81801c6c4
vaishalitekale <vaishali.tekale@gmail.com>
2015-10-14 15:16:34 +0530

    Added ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,23 +15,24 @@ angular
     'ngCookies',
     'ngMessages',
     'ngResource',
-    'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+	'ui.router'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
+  .config(['$stateProvider','$urlRouterProvider',
+     function ($stateProvider, $urlRouterProvider) {
+	
+	$urlRouterProvider.otherwise("/");
+    $stateProvider
+	  .state('main', {
+        url: '/',
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        controller: 'MainCtrl'
       })
-      .when('/about', {
+	  .state('about', {
+        url: '/about',
         templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+        controller: 'AboutCtrl'
       })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  }]);
+  
