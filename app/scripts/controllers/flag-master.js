aa5427a27c22a26886bfde7a58db3a02c9d5c012
vaishalitekale <vaishali.tekale@gmail.com>
2015-11-02 16:46:11 +0530

    error resolve

--- app/scripts/controllers/flag-master.js
+++ app/scripts/controllers/flag-master.js
@@ -8,16 +8,17 @@
  * Controller of the pmsApp
  */
 angular.module('pmsApp')
-  .controller('FlagMasterCtrl', ['$scope', '$http','flag',  function ($scope, $http, flag) {
+  .controller('FlagMasterCtrl', ['$scope', '$http',  function ($scope, $http) {
   		$scope.myData = {
   			columnDefs: [
     		  { name: 'flag_name'},
      		  { name: 'percent'},
      		  { name: 'amount'}
     		]
- 		 };	
+ 		};	
+
   		$http.get('http://localhost/PMS-1/services/index.php/flag')
   			.success(function (data) {
-   				 $scope.myData = data;
-  			});
+   				 $scope.myData = data.data;
+  		});
   }]);
