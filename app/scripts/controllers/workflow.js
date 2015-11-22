392c320fc2f9bba1d5bf94c2688f28fb7e9b6bb7
indrajitgestalt <indrajeetfazer@gmail.com>
2015-11-22 17:23:22 +0530

    workflow work

--- app/scripts/controllers/workflow.js
+++ app/scripts/controllers/workflow.js
@@ -15,7 +15,7 @@ angular.module('pmsApp')
       'Karma'
     ];
      $scope.items= [
-    {area:'cash'},
+    {id:'1',area:'cash'},
      {id:'2',area:'cheque'},
       {id:'3',area:'card'},
       
@@ -24,7 +24,71 @@ angular.module('pmsApp')
 
     ];
     $scope.showHideDiv = function () {
-if ($scope.chkStatus) {
+if ($scope.chkStatus1) {
+$scope.showhideprop = true;
+}
+else {
+$scope.showhideprop = false;
+}
+}
+ $scope.showHideDiv1 = function () {
+if ($scope.chkStatus2) {
+$scope.showhideprop = true;
+}
+else {
+$scope.showhideprop = false;
+}
+}
+ $scope.showHideDiv2 = function () {
+if ($scope.chkStatus3) {
+$scope.showhideprop = true;
+}
+else {
+$scope.showhideprop = false;
+}
+}
+$scope.showHideDiv3 = function () {
+if ($scope.chkStatus4) {
+$scope.showhideprop = true;
+}
+else {
+$scope.showhideprop = false;
+}
+}
+ $scope.showHideDiv4 = function () {
+if ($scope.chkStatus5) {
+$scope.showhideprop = true;
+}
+else {
+$scope.showhideprop = false;
+}
+}
+ $scope.showHideDiv5 = function () {
+if ($scope.chkStatus6) {
+$scope.showhideprop = true;
+}
+else {
+$scope.showhideprop = false;
+}
+}
+$scope.showHideDiv6 = function () {
+if ($scope.chkStatus7) {
+$scope.showhideprop = true;
+}
+else {
+$scope.showhideprop = false;
+}
+}
+ $scope.showHideDiv7 = function () {
+if ($scope.chkStatus8) {
+$scope.showhideprop = true;
+}
+else {
+$scope.showhideprop = false;
+}
+}
+ $scope.showHideDiv8 = function () {
+if ($scope.chkStatus9) {
 $scope.showhideprop = true;
 }
 else {
