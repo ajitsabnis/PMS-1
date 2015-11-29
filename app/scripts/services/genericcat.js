5d8d53189c938077c4b88420df443663a8bebe3c
Pravin.Patil02 <Pravin.Patil02@mphasis.com>
2015-11-29 16:10:35 +0530

    minar change in JS

--- app/scripts/services/genericcat.js
+++ app/scripts/services/genericcat.js
@@ -8,20 +8,18 @@
  * Service in the pmsApp.
  */
 
-
-angular.module('pmsApp').factory('genericService',['$resource', function ($resource) {
+angular.module('pmsApp').factory('genericService', ['$resource', function($resource) {
+  return{
+   		category:$resource( apiUrl.getSelectedCategoryData, {} ),
+  		update:$resource( apiUrl.updateGenericData, {} ),
+      deletecategory:$resource( apiUrl.deleteSelectedCategoryData, {} )
+  };
+}]);
+/*angular.module('pmsApp').factory('genericService',['$resource', function ($resource) {
     return $resource('http://localhost/PMS-1/services/index.php/instruments/instrument/', {}, {
       getGenericData: {method:'GET', params: {id: ''} , isArray:false},
-      post:  {method:'POST',  params: {} , isArray:true}
-
-
-    return{
-     		category:$resource( apiUrl.getSelectedCategoryData, {} ),
-    		update:$resource( apiUrl.updateGenericData, {} ),
-        deletecategory:$resource( apiUrl.deleteSelectedCategoryData, {} )
-    };
-
-}]);
+      post:  {method:'POST',  params: {} , isArray:true},
+}]);*/
 
 angular.module('pmsApp').factory('generic', ['$resource', 'apiUrl', function ($resource, apiUrl) {
 	return $resource( apiUrl.getGenericData, {} );
